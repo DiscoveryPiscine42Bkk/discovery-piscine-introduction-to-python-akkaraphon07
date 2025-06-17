a489d8b8e8a01c1dfdf7013ce0c0e5288279765c
akkaraphon07 <dannogod.11y@gmail.com>
2025-06-17 07:59:37 +0000

    python

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,7 +1,7 @@
 num = int(input())
-if num >0 :
+if num <0 :
     print("This number is negative")
-elif num <0 :
+elif num >0 :
     print("This number is positive")
 else:
     print("This number is both positive and negative")
---
+++ cell02/ex02/ password.py
@@ -0,0 +1,6 @@
+pwinput = input("")
+password = "1212312121"
+if pwinput == password :
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED") 
