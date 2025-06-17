f05c2841b0d2192659f1048049a81d6baf19a770
akkaraphon07 <dannogod.11y@gmail.com>
2025-06-17 04:47:11 +0000

    python

--- cell01/ex00/name.py
+++ cell01/ex00/name.py
@@ -1,3 +1,3 @@
 first_name = "Oakkharaphon"
 last_name = "Yindeeram"
-print(first_name + " " ++ last_name)
+print(first_name + last_name)
---
+++ cell01/ex02/age.py
@@ -0,0 +1,4 @@
+a = int(input())
+b = int(input())
+my_age = (a + b)
+print(my_age)
