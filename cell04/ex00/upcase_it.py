91144e08147644f077f5d33d5f7b5cf711738ffb
akkaraphon07 <dannogod.11y@gmail.com>
2025-06-18 04:54:13 +0000

    python

---
+++ cell04/ex00/upcase_it.py
@@ -0,0 +1,3 @@
+word = input("Please enter a word: ")
+upper_word = word.upper()
+print(upper_word)
---
+++ cell04/ex01/age.py
@@ -0,0 +1,4 @@
+age = int(input("Please enter your age: "))
+print("In 10 years, you will be", age + 10)
+print("In 20 years, you will be", age + 20)
+print("In 30 years, you will be", age + 30)
