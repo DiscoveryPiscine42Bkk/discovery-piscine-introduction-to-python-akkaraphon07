1e5a5db0d20a9f0354d33596631a540edce9732f
akkaraphon07 <dannogod.11y@gmail.com>
2025-06-19 03:35:32 +0000

    python

---
+++ cell05/ex13/append_it.py
@@ -0,0 +1,10 @@
+import sysAdd commentMore actions
+def main():
+    if len(sys.argv) == 1:
+        print("none")
+        return
+    for arg in sys.argv[1:]:
+        if not arg.endswith("ism"):
+            print(arg + "ism")
+if __name__ == "__main__":
+    main()
