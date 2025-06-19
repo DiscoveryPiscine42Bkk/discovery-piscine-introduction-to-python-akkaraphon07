ebb42c19e4de150f950d6ede233254f19a1ec90a
akkaraphon07 <dannogod.11y@gmail.com>
2025-06-19 08:32:06 +0000

    python

--- rush00/ex00/main.py checkmate.py
+++ rush00/ex00/main.py checkmate.py
@@ -1,25 +1,27 @@
-def checkmate(board_str):
+#ต้องการรู้ว่าคิงโดนอิสไหม
+# โดยที่ Pอิสได้ต่อเมื่ออยู่ข้างบน / B แนวทแยง / R หน้าหลังซ้ายขวา / Q รูปดอกจันทร์ 
+def checkmate(board_str): #แบ่งข้อความเป้นบันทัดเหมือนตารางหมากรุก
     board = [list(row) for row in board_str.splitlines()]
     n = len(board)
 
-    king_pos = None
+    king_pos = None #หาตำแหน่งว่ามี เค อยู่ในตารางไหมเก็บตัวแปลไว้ใน คิงพอต 
     for r in range(n):
         for c in range(n):
             if board[r][c] == 'K':
-                king_pos = (r, c)
+                king_pos = (r, c) #อา=แถว ซี=คอลั่ม
                 break
         if king_pos:
             break
-    if not king_pos:
+    if not king_pos: #ถ้าไม่เจอเคในตารางจะแสดงผลเฟล
         print("Fail")
         return
 
-    kr, kc = king_pos
+    kr, kc = king_pos 
     def inside(r, c):
         return 0 <= r < n and 0 <= c < n
 
     for dc in [-1, 1]:
-        pr, pc = kr - 1, kc + dc
+        pr, pc = kr - 1, kc + dc 
         if inside(pr, pc) and board[pr][pc] == 'P':
             print("Success")
             return
